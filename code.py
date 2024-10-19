6798ccc6f6f58c5afbce427062b6d5001b75d61d
Rishabh Goyal <86996785+rishug03@users.noreply.github.com>
2024-10-19 14:42:56 +0530

    Update code.py

--- code.py
+++ code.py
@@ -9,37 +9,31 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-# Step 1: Load the data.
-# Download the datsets from Kaggle 'Fake News Detection Datasets' and put in dataset directory.
-fake_df = pd.read_csv('dataset/Fake.csv')
-true_df = pd.read_csv('dataset/True.csv')
 
-# Step 2: Prepare the data
-fake_df['label'] = 0 # Label 0 is for Fake news
-true_df['label'] = 1 # Label 1 is for True news
+fake_df = pd.read_csv('E:\VS Code\Fake News\dataset/Fake.csv')
+true_df = pd.read_csv('E:\VS Code\Fake News\dataset/True.csv')
+
+
+fake_df['label'] = 0 
+true_df['label'] = 1 
 df = pd.concat([true_df, fake_df], axis=0).reset_index(drop=True)
 
-# Step 3: Preprocess the data: Create feature and target datasets.
-X = df['text'] # Feature
-y = df['label'] # Target
 
-# Step 4: Convert the text data into numerical form using TfidfVectorizer.
-# TfidfVectorizer depicts the importance and uniqueness of the word in the dataset.
-tf_idf_vectorizer = TfidfVectorizer(stop_words="english", max_df=0.7) # Ignore if the words appears in 70% or more of the documents.
+X = df['text'] 
+y = df['label']
+
+
+tf_idf_vectorizer = TfidfVectorizer(stop_words="english", max_df=0.7) 
 X_tf_idf = tf_idf_vectorizer.fit_transform(X)
 
-# Step 5: Split the Data.
-# Split into 80-20 ratio: Training (80%) and Validation(20%) datasets.
+
 X_train, X_val, y_train, y_val = train_test_split(X_tf_idf, y, test_size=0.2, random_state=42)
 
-# Step 6: Create and train the model
-# Each iteration allows the model to learn from mistakes it made in the previous iteration.
-# The PassiveAggressiveClassifier gets updated "aggressively" when it misclassifies an example,
-# adjusting its decision boundary to fix the mistake.
+
 model = PassiveAggressiveClassifier(max_iter=10)
 model.fit(X_train, y_train)
 
-# Step 7: Make prediction and Evaluate
+
 predictions = model.predict(X_val)
 accuracy_score = accuracy_score(y_val, predictions)
 print("Accuracy score:\n", accuracy_score)
@@ -49,7 +43,7 @@ classification_report = classification_report(y_val, predictions)
 print("Classfication Report:\n", classification_report)
 
 
-# Step 8: Visualization
+
 plt.figure(figsize=(7,5))
 sns.heatmap(confusion_matrix, annot=True, fmt='d', cmap='Blues')
 plt.xlabel('Predicted Values')
